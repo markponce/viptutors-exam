1949207899f21eef0e67e0a5573f1d9f533717ac
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-05 16:57:54 +0800

    User add products relationshipt and getProducts method

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    // Define the one-to-many relationship in the User and Product models. 
+    public function products(): HasMany {
+        return $this->hasMany(Product::class);
+    }
+    
+    // Write a method in the User model to fetch all products of a user.
+    public function getProducts(){
+        return $this->products();
     }
 }
