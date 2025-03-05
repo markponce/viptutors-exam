dab93894ea37ee2c00518b932d3a2b88d74c68ea
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-05 16:57:13 +0800

    Call UserSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'is_admin' => 1,
-            'password' => Hash::make('password')
-        ]);
+        // run user seeder
+        $this->call(UserSeeder::class);
     }
 }
