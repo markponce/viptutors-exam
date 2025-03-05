3b47c480eedec688aeebe54a9fbc7a6871bd197d
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-05 18:47:03 +0800

    DatabaseSeeder add ProductSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // run user seeder
-        $this->call(UserSeeder::class);
+        $this->call([
+            UserSeeder::class, 
+            ProductSeeder::class,
+        ]);
     }
 }
