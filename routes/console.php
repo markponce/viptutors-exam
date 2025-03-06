805de3928c0f67dd6ec8834d1d6777bb59d5e59e
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-07 06:51:54 +0800

    Create a scheduled job

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::call(function () {
+    // Can be created as a Job
+    Product::where('quantity', '<', 10)->delete();
+})->mondays()->at('00:00');
