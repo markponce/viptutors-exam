a42fe0679640af4652f157961b62a801cda74116
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 22:28:50 +0800

    Hide Dashboard Nav if user is not admin and add product link on nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,13 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    @if (Auth::user()->isAdmin() === true)
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                    @endif
+                    <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.*')">
+                        {{ __('Products') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -67,9 +72,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            @if (Auth::user()->isAdmin() === true)
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>    
+            @endif
+            <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.*')">
+                {{ __('Products') }}
+            </x-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
