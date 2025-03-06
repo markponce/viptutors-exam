67d5ddb046c31d638cc307a1ff0a484f0232d352
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 22:55:15 +0800

    Remove welcom page

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ use App\Http\Middleware\CheckAdmin;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['auth', 'verified',])->group(function () {
     Route::middleware([CheckAdmin::class,])->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index'])
