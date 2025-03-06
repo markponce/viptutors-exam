a0617676c47093ca7134e787e8a78c999e47d3e3
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 22:30:25 +0800

    Add table list of users in Dashboard blade view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Users') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,72 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Is Admin
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($users as $user)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{$user->id}}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{$user->name}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{$user->email}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                            
+                                        @if ($user->isAdmin() === true)
+                                            <svg class="w-3 h-3 text-green-500" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                            </svg>
+                                        @else
+                                            <svg class="w-3 h-3 text-yellow-500" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href="{{route("product.index")}}" rel="noopener noreferrer" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            Products
+                                        </a>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr class="">
+                                    <td colspan="5" class="px-6 py-4 text-center">No Records</td>
+                                </tr>
+                                @endforelse
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
