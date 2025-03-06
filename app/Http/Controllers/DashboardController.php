bd1afbdf19e0d094beb92290805f50f5e12a94a2
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 21:36:40 +0800

    Create DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('dashboard', [
+            'users' => $users,
+        ]);    
+    }
+}
