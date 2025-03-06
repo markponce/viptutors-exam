fae96d822b9fbe83bcf0c10f92ca42ee3695fb24
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 22:25:15 +0800

    Add Product route and set group middleware

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,38 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckAdmin;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified',])->group(function () {
+    Route::middleware([CheckAdmin::class,])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->name('dashboard');
+    });
+    Route::prefix('/products')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])
+            ->name('product.index');
+        Route::get('/create', [ProductController::class, 'create'])
+            ->name('product.create');
+        Route::get('/{product}', [ProductController::class, 'show'])
+            ->name('product.show');
+        Route::post('/products', [ProductController::class, 'store'])
+            ->name('product.store');
+        Route::get('/edit/{product}', [ProductController::class, 'edit'])
+            ->name('product.edit');
+        Route::delete('/{product}', [ProductController::class, 'destroy'])
+            ->name('product.destroy');
+        Route::patch('/edit/{product}', [ProductController::class, 'update'])
+            ->name('product.update');
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +40,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
