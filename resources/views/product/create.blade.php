9a8fefeaebb3a355e77356056cc35bc1c4f1692d
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-06 22:26:48 +0800

    Create product blade views

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Product Create Form') }}
+            </h2>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('product.store') }}">
+                        @csrf
+
+                        <!-- Title -->
+                        <div>
+                            <x-input-label for="title" :value="__('Title')" />
+                            <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
+                                :value="old('title')" required autofocus autocomplete="title" />
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                        </div>
+
+                        <!-- Body -->
+                        <div class="mt-4">
+                            <x-input-label for="body" :value="__('Body')" />
+                            {{-- <x-text-input id="body" class="block mt-1 w-full" type="text" name="body"
+                                :value="old('body')" required autocomplete="body" /> --}}
+                            <textarea id="body"
+                                class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                                name="body" required autocomplete="body">{{old('body')}}</textarea>
+                            <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                        </div>
+                        <div class="flex items-center justify-end mt-4">
+                            <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-4" href="{{route('product.index')}}">Cancel</a>
+                            <x-primary-button class="ms-4">
+                                {{ __('Save') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,72 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Product List') }}
+            </h2>
+            <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-4" href="{{route('product.create')}}">Create</a>
+        </div>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="relative overflow-x-auto">
+                        <x-auth-session-status class="mb-4 text-center" :status="session('status')" />
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Title
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Body
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        User
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $product)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{$product->id}}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{$product->title}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{$product->body}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{$product->user->name}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="{{route('product.show', ['product' => $product->id])}}" rel="noopener noreferrer" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            View
+                                        </a>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr class="">
+                                    <td colspan="5" class="px-6 py-4 text-center">No Records</td>
+                                </tr>
+                                @endforelse
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
