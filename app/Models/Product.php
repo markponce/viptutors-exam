d359901c2f5a0c8a05379d84834e5c7335322a30
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-07 02:01:00 +0800

    Create ProductObserver

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Models\Scopes\DeletedScope;
+use App\Observers\ProductObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 
 #[ScopedBy([DeletedScope::class])]
+#[ObservedBy([ProductObserver::class])]
 class Product extends Model
 {
     protected $fillable = [
---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Events\ProductCreated;
+use App\Models\Product;
+use Illuminate\Support\Facades\Log;
+
+class ProductObserver
+{
+    /**
+     * Handle the Product "created" event.
+     */
+    public function created(Product $product): void
+    {
+        Log::debug('Product Created!');
+        event(new ProductCreated($product));
+        
+    }
+
+    /**
+     * Handle the Product "updated" event.
+     */
+    public function updated(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "deleted" event.
+     */
+    public function deleted(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "restored" event.
+     */
+    public function restored(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "force deleted" event.
+     */
+    public function forceDeleted(Product $product): void
+    {
+        //
+    }
+}
